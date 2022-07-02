c8ecc7cf9b2bbc3a3821cb7357c9de0f13b75f1f
choir27 <richardchoi54@gmail.com>
2022-07-02 17:02:03 -0400

    added js logic for closing and opening menu

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let menu = document.querySelector('#menu')
+let close = document.querySelector('#close')
+
+menu.addEventListener('click',showMenu)
+
+function showMenu(){
+    document.querySelector('.nav').classList.remove('hidden')
+    menu.classList.add('hidden')
+}
+
+close.addEventListener('click',closeMenu)
+
+function closeMenu(){
+    menu.classList.remove('hidden')
+    document.querySelector('.nav').classList.add('hidden')
+}
