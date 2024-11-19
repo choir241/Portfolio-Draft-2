00e1c9830112f198a79129ea5a8d617b0527318e
Richard <richardchoi54@gmail.com>
2024-11-19 14:23:23 -0500

    Fix iframe errors

--- app/blogs-and-streams/page.tsx
+++ app/blogs-and-streams/page.tsx
@@ -131,7 +131,7 @@ export default function Blog() {
     {
     src: stream4,
     altlink: (
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/wcJVHQJQXzg?si=uLdErYuc3rYe1gGF" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/wcJVHQJQXzg?si=uLdErYuc3rYe1gGF" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
     ),
     link: "https://www.youtube.com/watch?v=wcJVHQJQXzg&ab_channel=KrishCodes",
     title: "Podcast 404"
@@ -226,7 +226,7 @@ export default function Blog() {
         Streams I have been on
       </h2>
 
-      <div className="flex items-center max-[1200px]:flex-col w-full justify-evenly mb-16">
+      <div className="flex items-center max-[600px]:flex-col w-full justify-evenly mb-16">
         {streams.map((streamObj: IStream) => {
           return (
             <section
@@ -239,7 +239,7 @@ export default function Blog() {
         })}
       </div>
 
-      <div className="flex items-center max-[1200px]:flex-col w-full justify-evenly">
+      <div className="flex items-center max-[600px]:flex-col w-full justify-evenly">
         {streams1.map((streamObj: IStream) => {
           return (
             <section
@@ -252,7 +252,7 @@ export default function Blog() {
         })}
       </div>
 
-      <div className="flex items-center max-[1200px]:flex-col w-full justify-evenly">
+      <div className="flex items-center max-[600px]:flex-col w-full justify-evenly">
         {streams2.map((streamObj: IStream) => {
           return (
             <section
